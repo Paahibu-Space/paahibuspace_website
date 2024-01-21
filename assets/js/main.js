7357e98f3db21d351bf0e1cb8630b21e94d65112
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-01-21 22:06:02 +0000

    fixed mobile-navbar issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,41 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+
+    /**
+   * Mobile nav toggle
+   */
+    const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
+    if (mobileNavToogle) {
+      mobileNavToogle.addEventListener('click', function(event) {
+        event.preventDefault();
+  
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+  
+        this.classList.toggle('bi-list');
+        this.classList.toggle('bi-x');
+      });
+    }
+  
+    /**
+     * Toggle mobile nav dropdowns
+     */
+    const navDropdowns = document.querySelectorAll('.navbar-nav .dropdown > a');
+  
+    navDropdowns.forEach(el => {
+      el.addEventListener('click', function(event) {
+        if (document.querySelector('.mobile-nav-active')) {
+          event.preventDefault();
+          this.classList.toggle('active');
+          this.nextElementSibling.classList.toggle('dropdown-active');
+  
+          let dropDownIndicator = this.querySelector('.dropdown-indicator');
+          dropDownIndicator.classList.toggle('bi-chevron-up');
+          dropDownIndicator.classList.toggle('bi-chevron-down');
+        }
+      })
+    });
+
     /**
    * Hero Slider
    */
@@ -147,40 +182,6 @@ document.addEventListener('DOMContentLoaded', () => {
         scrollto(window.location.hash);
       }
     }
-  });
-
-  /**
-   * Mobile nav toggle
-   */
-  const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
-  if (mobileNavToogle) {
-    mobileNavToogle.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      document.querySelector('body').classList.toggle('mobile-nav-active');
-
-      this.classList.toggle('bi-list');
-      this.classList.toggle('bi-x');
-    });
-  }
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
-  navDropdowns.forEach(el => {
-    el.addEventListener('click', function(event) {
-      if (document.querySelector('.mobile-nav-active')) {
-        event.preventDefault();
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('dropdown-active');
-
-        let dropDownIndicator = this.querySelector('.dropdown-indicator');
-        dropDownIndicator.classList.toggle('bi-chevron-up');
-        dropDownIndicator.classList.toggle('bi-chevron-down');
-      }
-    })
   });
 
   
