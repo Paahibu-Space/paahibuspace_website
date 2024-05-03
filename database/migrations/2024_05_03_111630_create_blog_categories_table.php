64d9a533e8dc98e7d903a9c7126e5997694df952
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-05-03 11:20:27 +0000

    blog categories schema created

--- database/migrations/2024_05_03_111630_create_blog_categories_table.php
+++ database/migrations/2024_05_03_111630_create_blog_categories_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs_categories', function (Blueprint $table) {
+        Schema::create('blog_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
