5e6af56f11e413f850a7cfcb741d543653398cd6
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-05-11 16:49:55 +0000

    team functionality added

---
+++ app/Http/Controllers/Backend/TeamMemberController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\TeamMember;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class TeamMemberController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    public function index()
+    {
+        $all_team_member = TeamMember::all();
+        return view('backend.team.team-member')->with(['all_team_member' => $all_team_member]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:191',
+            'designation' => 'required|string|max:191',
+            'image' => 'nullable|string|max:191',
+            'icon_one' => 'nullable|string|max:191',
+            'icon_two' => 'nullable|string|max:191',
+            'icon_three' => 'nullable|string|max:191',
+            'icon_one_url' => 'nullable|string|max:191',
+            'icon_two_url' => 'nullable|string|max:191',
+            'icon_three_url' => 'nullable|string|max:191'
+        ]);
+        TeamMember::create($request->all());
+
+        return redirect()->back()->with(['msg' => __('New Team Member Added...'), 'type' => 'success']);
+    }
+
+    public function update(Request $request)
+    {
+
+
+        $this->validate($request, [
+            'name' => 'required|string|max:191',
+            'designation' => 'required|string|max:191',
+            'image' => 'nullable|string|max:191',
+            'icon_one' => 'nullable|string|max:191',
+            'icon_two' => 'nullable|string|max:191',
+            'icon_three' => 'nullable|string|max:191',
+            'icon_one_url' => 'nullable|string|max:191',
+            'icon_two_url' => 'nullable|string|max:191',
+            'icon_three_url' => 'nullable|string|max:191'
+        ]);
+        TeamMember::find($request->id)->update($request->all());
+
+        return redirect()->back()->with(['msg' => __('Team Member Details Updated...'), 'type' => 'success']);
+    }
+
+    public function delete($id)
+    {
+       TeamMember::find($id)->delete();
+        return redirect()->back()->with(['msg' => __('Delete Success...'), 'type' => 'danger']);
+    }
+
+    public function bulk_action(Request $request){
+        TeamMember::whereIn('id',$request->ids)->delete();
+        return response()->json(['status' => 'ok']);
+    }
+}
